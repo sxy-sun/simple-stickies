a1e9c879704cf1bb8c01809fa5b62c3167f25504
Shaw Sun <sxy-sun@users.noreply.github.com>
2026-08-15 20:54:09 -0400

    Fix cask checksum

--- Casks/simple-stickies.rb
+++ Casks/simple-stickies.rb
@@ -1,6 +1,6 @@
 cask "simple-stickies" do
   version "1.0.0"
-  sha256 "4d2d014abd033c967f9ea36e4d3bb3fc7b2185c7da091d00ca4ed096147546ad"
+  sha256 "2b8f2950def35430021efc7403d132aebe20522df4b8e896d97f77a0c62f06c4"
 
   url "https://github.com/sxy-sun/simple-stickies/releases/download/v#{version}/SimpleStickies.zip"
   name "Simple Stickies"
